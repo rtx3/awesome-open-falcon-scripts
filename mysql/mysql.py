972e539e735d419f08b6211debef0ba4cad1af3d
fenghua <hua.feng@unionpayadvisors.com>
2018-01-30 16:59:28 +0800

    mysql script

--- mysql/mysql.py
+++ mysql/mysql.py
@@ -4,9 +4,11 @@
 
 
 from os import popen as ctl
-import pub
+from time import time
+import json
+import httplib
 
-
+PUSH_PATH = "127.0.0.1:1988"
 DB_host = "127.0.0.1"
 DB_port = 3306
 DB_user = "root"
@@ -22,6 +24,42 @@ METRICS = ['Questions',
            'Com_commit',
            'Com_rollback',
            'Uptime']
+
+
+class P_data(object):
+    def __init__(self):
+        self.result = []
+        self.Endpoint = get_endpoint()
+        self.ts = int(time())
+
+    def add(self, metric, value, tag, counterType="GAUGE", step=60):
+        self.result.append({
+            "endpoint": self.Endpoint,
+            "metric": metric,
+            "timestamp": self.ts,
+            "step": step,
+            "value": value,
+            "counterType": counterType,
+            "tags": tag,
+        })
+
+    def get(self):
+        return self.result
+
+    def out(self):
+        return json.dumps(self.result)
+
+    def push(self):
+        data = json.dumps(self.result)
+        h = httplib.HTTPConnection(PUSH_PATH)        
+        h.request('POST', '/v1/push', data)        
+        r = h.getresponse()        
+        print r.read()
+
+
+def get_endpoint():
+    from socket import gethostname
+    return gethostname()
 
 
 def get_mysql_status():
