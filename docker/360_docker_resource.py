aa1ff8687bec607312b3cdb6aa0d781197cbd4e2
fenghua <hua.feng@unionpayadvisors.com>
2018-01-31 12:56:48 +0800

    docker  script

---
+++ docker/360_docker_resource.py
@@ -0,0 +1,104 @@
+#!/bin/env python
+#-*- coding:utf-8 -*-
+
+import os,sys
+import os.path
+from os.path import isfile
+from traceback import format_exc
+import xmlrpclib
+import socket
+import time
+import json
+import copy
+import httplib
+
+
+PUSH_PATH = "127.0.0.1:1988"
+PREFIX = "docker."
+TAGS = ""
+STEP = 360
+
+
+class Resource():
+    def __init__(self, metrics, tag=None):
+        self.host = socket.gethostname()
+        self.metrics = metrics
+        self.resources_d = {}
+        self.tag = TAGS
+
+    def run(self):
+        for metric in self.metrics:
+            resource_list_cpu = []
+            resource_list_mem = []
+            for key in metric:
+                resource_list_cpu.append(metric[key][0])
+                resource_list_cpu.append('GAUGE')
+                self.resources_d[PREFIX + key + '.CPU'] = resource_list_cpu
+                resource_list_mem.append(metric[key][1])
+                resource_list_mem.append('GAUGE')
+                self.resources_d[PREFIX + key + '.MEM'] = resource_list_mem
+        output = []
+        for resource in self.resources_d.keys():
+                t = {}
+                t['endpoint'] = self.host
+                t['timestamp'] = int(time.time())
+                t['step'] = STEP
+                t['counterType'] = self.resources_d[resource][1]
+                t['metric'] = resource
+                t['value'] = self.resources_d[resource][0]
+                t['tags'] = "%s" % (self.tag)
+
+                output.append(t)
+
+        return output
+
+    def dump_data(self):
+        return json.dumps()
+
+
+def push(data):
+    data = json.dumps(data)
+    headers = {"Content-type": "application/json", "Accept": "text/plain"}   
+    try:
+        h = httplib.HTTPConnection(PUSH_PATH)        
+        h.request('POST', '/v1/push', data, headers)
+    except Exception:
+        print "Pushing Failed when sending data."
+        return 1
+    print "Pushing finished."        
+    return 0
+
+
+def get_resources():
+    cmd = "docker stats --no-stream|\
+           awk 'NR>1{print $2, $3, $7}'"
+    ret = []
+    for item in os.popen(cmd).readlines():
+        resource = {}
+        resource[int(item.split()[0])] = [item.split()[1].strip("\n").strip("%"),
+                                          item.split()[2].strip("\n").strip("%")]
+        ret.append(resource)
+    return ret
+
+if __name__ == "__main__":
+    resources = get_resources()
+    print resources
+    print "Pushing...."
+    d = Resource(metrics=resources).run()
+    print d
+    #if d:
+    #    push(d)
+    #else:
+    #    print "Pushing Failed."
+            #if d:
+            #    print "OK. " + str(idx)
+            #else:
+    #for idx, item in enumerate(resources):
+    #    for resource in item:            
+    #        print "Pushing" + str(resource) 
+            #d = Resource(metric=resource, tag=item[resource]).run()
+            #if d:
+            #    print "OK. " + str(idx)
+            #else:
+            #    print "Fail." + str(idx)
+
---
+++ mesos/360_TODO_health_check.py
@@ -0,0 +1,90 @@
+#!/bin/env python
+#-*- coding:utf-8 -*-
+
+import os
+import os.path
+import socket
+import time
+import json
+
+import httplib
+
+
+PUSH_PATH = "127.0.0.1:1988"
+API_PATH = "localhost:8088"
+PREFIX = "hadoop."
+TAGS = ""
+API = "/ws/v1/cluster/metrics"
+STEP = 360
+
+
+class Resource():
+    def __init__(self, metrics, tag=None):
+        self.host = socket.gethostname()
+        self.metrics = metrics
+        self.resources_d = {}
+        self.tag = TAGS
+
+    def run(self):
+        for key in self.metrics.keys():
+            resource_list = []
+            resource_list.append(self.metrics[key])
+            resource_list.append('GAUGE')
+            self.resources_d[PREFIX + key] = resource_list
+        output = []
+        for resource in self.resources_d.keys():
+                t = {}
+                t['endpoint'] = self.host
+                t['timestamp'] = int(time.time())
+                t['step'] = STEP
+                t['counterType'] = self.resources_d[resource][1]
+                t['metric'] = resource
+                t['value'] = self.resources_d[resource][0]
+                t['tags'] = "%s" % (self.tag)
+
+                output.append(t)
+
+        return output
+
+
+def push(data):
+    data = json.dumps(data)
+    headers = {"Content-type": "application/json", "Accept": "text/plain"}   
+    try:
+        h = httplib.HTTPConnection(PUSH_PATH)        
+        h.request('POST', '/v1/push', data, headers)
+    except Exception:
+        print "Pushing Failed when sending data."
+        return 1
+    print "Pushing finished."        
+    return 0
+
+
+def get_resources():
+    cmd = "curl -s http://" + API_PATH + API
+    result = os.popen(cmd).readlines()
+    ret = json.loads(result[0])['clusterMetrics']
+    return ret
+
+if __name__ == "__main__":
+    resources = get_resources()
+    print resources, type(resources)
+    print "Pushing...."
+    d = Resource(metrics=resources).run()
+    print d
+    #if d:
+    #    push(d)
+    #else:
+    #    print "Pushing Failed."
+            #if d:
+            #    print "OK. " + str(idx)
+            #else:
+    #for idx, item in enumerate(resources):
+    #    for resource in item:            
+    #        print "Pushing" + str(resource) 
+            #d = Resource(metric=resource, tag=item[resource]).run()
+            #if d:
+            #    print "OK. " + str(idx)
+            #else:
+            #    print "Fail." + str(idx)
+
