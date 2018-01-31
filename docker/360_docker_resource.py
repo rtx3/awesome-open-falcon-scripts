5a295ad16557e0c908b43580187b8fe36862de60
fenghua <hua.feng@unionpayadvisors.com>
2018-01-31 14:02:02 +0800

    docker  script

--- docker/360_docker_resource.py
+++ docker/360_docker_resource.py
@@ -85,6 +85,7 @@ def get_resources():
 def check_docker_version():
     cmd = "docker -v|awk '{print $3}'"
     result = os.popen(cmd).readlines()
+    print result
     docker_version = result.split('.')
     print docker_version
 
